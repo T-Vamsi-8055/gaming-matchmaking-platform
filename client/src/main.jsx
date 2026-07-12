c04b7d37311ff5bf2ea411a44da070eee0b855c6
Gaurav Gaisenn <gauravgaisenn@gmail.com>
2026-07-12 21:15:36 +0530

    feat: add /profile route and clean up auth imports

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,9 @@
 import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Login from './auth/isLogin.jsx' 
-import VerifyOtp from './auth/VerifyOTP.jsx' 
+import Login from './isLogin.jsx'
 import Home from './home.jsx' 
+import Profile from './profile.jsx'
 import OtpVerify from "./otpVerify.jsx";
 import {  BrowserRouter,Routes,Route } from 'react-router-dom';
 
@@ -14,6 +14,7 @@ createRoot(document.getElementById('root')).render(
         <Route path="/" element={<Home />}></Route>
         <Route path="/auth" element={<Login />}></Route>
         <Route path="/otpVerify" element={<OtpVerify />}></Route>
+        <Route path="/profile" element={<Profile />}></Route>
       </Routes>
     </BrowserRouter>
   </StrictMode>
