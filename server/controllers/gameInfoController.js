da0794bb05b1a98847b80264f38c3c6e45952d69
T-Vamsi-8055 <ivamsi776@gmail.com>
2026-07-15 10:32:00 +0530

    Added the working controller for the game data management.

---
+++ server/controllers/gameInfoController.js
@@ -0,0 +1,29 @@
+import { pool } from "../config/db.js";
+
+async function handleGameInfo(req,res){
+    const gameName=req.params.game_name;
+    const userId=req.user.id;
+    try{
+        const response=await pool.query("select gamer_id from profiles where user_id=$1 ",[userId]);
+        if (response.rows.length === 0) {
+            return res.status(404).json({ message: "Profile not found" });
+        }
+        const gamerId=response.rows[0].gamer_id;
+        const gameData=await pool.query("select * from mock_game_data where gamer_id=$1 and game_name=$2",[gamerId,gameName]);
+        if (gameData.rows.length === 0) {
+            return res.status(404).json({ message: "Game data not found" });
+        }
+        return res.status(200).json({
+            gameData:gameData.rows[0]
+        })
+
+    }catch(err){
+        console.log(err)
+        
+        return res.status(500).json({
+            message:"Server side error"
+        })
+        
+    }
+}
+export {handleGameInfo}
