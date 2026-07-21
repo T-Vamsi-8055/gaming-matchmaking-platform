7dc2220da1739b16778b5b44c10a4968b8cb1da4
T-Vamsi-8055 <ivamsi776@gmail.com>
2026-07-21 16:17:56 +0530

    feat: Added the party controller and party routes

--- client/src/party.jsx
+++ client/src/party.jsx
@@ -1,11 +1,33 @@
 import React from 'react'
 
+const API_PORT=3000
 const party = () => {
+    const handleJoinParty=()=>{
+        try {
+            const response=await fetch(`http://localhost:${API_PORT}/api/join-party`);
+        }catch(err){
+            console.log("Error",err);
+        }
+    }
+    const handleCreateParty=()=>{
+        try {
+            const response=await fetch(`http://localhost:${API_PORT}/api/create-party`);
+        }catch(err){
+            console.log("Error",err);
+        }
+    }
   return (
     <div>
       <div>
-        <label htmlFor="partName" >Enter the party code to join</label><input name="partName" type="text" />
-
+        <form onSubmit={handleJoinParty}>
+        <label htmlFor="partName" >Enter the party code to join</label><input name="partName" type="text" /><button type="submit">Join Party</button>
+        </form>
+        <form onSubmit={}>
+            <h2>Create Party</h2>
+            <button type="radio">Public</button>
+            <button type="radio">Private</button>
+            <button type="submit">Create Party</button>
+        </form>
       </div>
     </div>
   )
